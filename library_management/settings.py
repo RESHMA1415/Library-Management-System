4a03109201b8ab1bae5ddd4f7f6fb9dc4698923e
RESHMA <reshmarajan481@gmail.com>
2026-07-01 10:18:51 +0530

    Update settings for deployment

--- library_management/settings.py
+++ library_management/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0t6kvkf^s%$b(+o8=xxxc2y1$ha@wi$7zym*1gup9_(^qag^(_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["reshma1415.pythonanywhere.com",]
 
 
 # Application definition
@@ -118,4 +118,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 AUTH_USER_MODEL = 'accounts.CustomUser'
